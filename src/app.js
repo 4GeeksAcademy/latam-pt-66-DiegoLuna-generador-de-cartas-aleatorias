683222bec983b409e921646c9d328357cb29198d
DreenLoon <diegoarmando.l.f@outlook.com>
2026-04-08 12:44:35 +0000

    Programe la idea que tengo del codigo para el proyecto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import "bootstrap";
 import "./style.css";
 
+let figures = ['♦', '♥', '♠', '♣']
+let numbers = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = function randomCard() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardBack = document.querySelector('#cardbg')
+  let ranFigures = figures[Math.floor(Math.random()*4)]
+  let ranNumbers = numbers[Math.floor(Math.random()*13)]
+  let ranFigures2 = ranFigures
+  cardBack.innerHTML = ranFigures + ranNumbers + ranFigures2
 };
